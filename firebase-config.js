2c2e7686808ddcf02ef6bc6f68483a5bab7c6968
mateomidtrod-commits <mateo.midtrod@icloud.com>
2026-03-18 19:35:46 +0100

    Update Firebase configuration with project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// ✏️ PASTE YOUR FIREBASE CONFIG HERE (see README.md for instructions)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDEBOYIQRzgURm5xk710YXLptXOS2Qnydg",
+  authDomain: "gruppechat-5d3f0.firebaseapp.com",
+  projectId: "gruppechat-5d3f0",
+  storageBucket: "gruppechat-5d3f0.firebasestorage.app",
+  messagingSenderId: "447233770228",
+  appId: "1:447233770228:web:8ad6ce4b0cf81b7b2c14a1"
 };
